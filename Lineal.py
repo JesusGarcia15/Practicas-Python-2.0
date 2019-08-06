20c9b3754c54b4433c4bf55be06af874d52292e7
JesusGarcia15 <52469539+JesusGarcia15@users.noreply.github.com>
2019-08-05 22:34:38 -0500

    Add files via upload

--- Lineal.py
+++ Lineal.py
@@ -1,7 +1,8 @@
 from numpy import*
+from sympy import*
 def gaussjordan1(a,b):
     n = len(b)
-    c = concatenate([a,b,d],axis = 1)
+    c = concatenate([a,b],axis = 1)
     for e in range(n):
         t=c[e,e]
         for j in range(e,n+1):
@@ -55,13 +56,83 @@ def Gauss1(a,b):
         t=c[e,e]
         for j in range(e,n+1):
             c[e,j]=c[e,j]/t
-        for i in range(e,n):
-            if i!=e:
+        for i in range(e+1,n):
                 t=c[i,e]
                 for j in range(e,n+1):
                     c[i,j]=c[i,j]-t*c[e,j]
     print(c)
-    for i in range (n,1):
-        x = 0
-        for j in range(i,n):
-            x = x + 
+    x = zeros([n,1])
+    x[n-1]=c[n-1,n]
+    for i in range (n-2,-1,-1):
+        s = 0
+        for j in range(i+1,n):
+            s=s+c[i,j]*x[j]
+        x[i]=c[i,n]-s
+    return x
+
+def jacobi (a,b,x):
+    n=len(x)
+    t=x.copy()
+    for i in range(n):
+        s=0
+        for j in range(n):
+            if i!=j:
+                s=s+a[i,j]*t[j]
+        x[i]=(b[i]-s)/a[i,i]
+    return x
+
+def jacobim (a,b,x,e,m):
+    n=len(x)
+    t=x.copy()
+    for k in range(m):
+        x=jacobi(a,b,x)
+        d=linalg.norm(array(x)-array(t))
+        print(x)
+        if d<e:
+            return [x,k]
+        else:
+            t=x.copy()
+    return [[],m]
+
+def gaussidel(a,b,x):
+    n=len(x)
+    for i in range (n):
+        s=0
+        for j in range (n):
+            if i!=j:
+                s=s+a[i,j]*x[j]
+        x[i]=(b[i]-s)/a[i][i]
+    return x
+
+def gaussidelm (a,b,x,e,m):
+    n=len(x)
+    t=x.copy()
+    for k in range(m):
+        x=gaussidel(a,b,x)
+        d=linalg.norm(array(x)-array(t))
+        if d<e:
+            return [x,k]
+        else:
+            t=x.copy()
+    return [[],m]
+
+def lagrange(x,y,u=None):
+    n=len(x)
+    t=Symbol('t')
+    p=0
+    for i in range(n):
+        L=1
+        for j in range(n):
+            if j!=i:
+                L=L*(t-x[j])/(x[i]-x[j])
+        p=p+y[i]*L
+        p=expand(p)
+    if u==None:
+        return p
+    elif type(u) == list:
+        v=[]
+        for i in range (len(u)):
+            v=v+[p.subs(t,u[i])]
+        return v
+    else:
+        return p.subs(t,u)
